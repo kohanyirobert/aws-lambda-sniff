33b5c6801189d50a2b246dc2667a97e0fada8910
Kohányi Róbert <kohanyi.robert@gmail.com>
2017-04-28 13:01:50 +0000

    Update VERSION, pimp up upload.sh to not allow overwriting zips

--- upload.sh
+++ upload.sh
@@ -1,2 +1,10 @@
 #! /bin/sh
-aws s3 cp package-$(basename $(pwd))-$(cat VERSION).zip s3://aws-lambda-deployment-packages
+bucket=aws-lambda-deployment-packages
+key=package-$(basename $(pwd))-$(cat VERSION).zip
+aws s3api head-object --bucket $bucket --key $key
+if [ $? -ne 0 ]
+then
+  aws s3 cp $key s3://$bucket
+else
+  echo $key already exists
+fi
