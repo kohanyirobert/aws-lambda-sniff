befdcf2e76ba980ba79e1e4a34cb5d798e7ee4cc
Kohányi Róbert <kohanyi.robert@gmail.com>
2019-07-10 21:20:34 +0200

    Update .travis.yml

--- travis.sh
+++ travis.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
-pip3 install --user awscli
 pip3 install --requirement requirements.txt --target .
 zip -r package-$(basename $(pwd))-$(cat VERSION).zip VERSION bs1770gain ffmpeg ffprobe tagit youtube_dl handler.py
 aws lambda update-function-code --function-name $(basename $(pwd)) --zip-file package-$(basename $(pwd))-$(cat VERSION).zip
