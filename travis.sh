3fa1811bb34625dc518d257e81fdb089a947026d
Kohányi Róbert <kohanyi.robert@gmail.com>
2019-07-10 22:27:18 +0200

    Update travis.sh, .gitignore and README

--- travis.sh
+++ travis.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-NAME=aws-lambda-sniff
+NAME=$(basename $(pwd))
 PACKAGE=package-$NAME-$(cat VERSION).zip
 pip3 install --requirement requirements.txt --target .
 zip -r $PACKAGE VERSION bs1770gain ffmpeg ffprobe tagit youtube_dl handler.py
