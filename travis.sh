a2984c40462b254bf215767168a8802e0e757f84
Kohányi Róbert <kohanyi.robert@gmail.com>
2019-07-10 22:06:42 +0200

    Update .travis.yml

--- travis.sh
+++ travis.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
+NAME=aws-lambda-sniff
+PACKAGE=package-$NAME-$(cat VERSION).zip
 pip3 install --requirement requirements.txt --target .
-zip -r package-$(basename $(pwd))-$(cat VERSION).zip VERSION bs1770gain ffmpeg ffprobe tagit youtube_dl handler.py
-aws lambda update-function-code --function-name $(basename $(pwd)) --zip-file fileb://package-$(basename $(pwd))-$(cat VERSION).zip
+zip -r $PACKAGE VERSION bs1770gain ffmpeg ffprobe tagit youtube_dl handler.py
+aws lambda update-function-code --function-name $NAME --zip-file fileb://$PACKAGE
