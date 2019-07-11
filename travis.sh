ac9a4b8224da9b32498775569ec94f7794b0cb0e
Kohányi Róbert <kohanyi.robert@gmail.com>
2019-07-11 10:06:19 +0000

    Update build/deploy script to make binaries executable

--- travis.sh
+++ travis.sh
@@ -2,11 +2,15 @@
 NAME=$(basename $(pwd))
 PACKAGE_PREFIX=package-
 PACKAGE=$PACKAGE_PREFIX$NAME-$(cat VERSION).zip
+BINARIES="bs1770gain ffmpeg ffprobe tagit"
+
+# INFO: `youtube-dl` can't find/use non-executable binaries.
+chmod -v +x $BINARIES
 
 rm -vrf youtube_dl*
 pip3 install --requirement requirements.txt --target .
 
 rm -vf $PACKAGE_PREFIX*.zip
-zip -r $PACKAGE VERSION bs1770gain ffmpeg ffprobe tagit youtube_dl handler.py
+zip -r $PACKAGE VERSION $BINARIES youtube_dl handler.py
 
 aws lambda update-function-code --function-name $NAME --zip-file fileb://$PACKAGE
