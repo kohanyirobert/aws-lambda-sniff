4fe09f2a0206db84e4a5cf1de034cfefaab43dcb
Kohányi Róbert <kohanyi.robert@gmail.com>
2019-07-11 13:21:48 +0200

    Fix chmod, bs1770gain was a folder, not a file

--- travis.sh
+++ travis.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 NAME=$(basename $(pwd))
-PACKAGE_PREFIX=package-
-PACKAGE=$PACKAGE_PREFIX$NAME-$(cat VERSION).zip
-BINARIES="bs1770gain ffmpeg ffprobe tagit"
+PREFIX=package-
+PACKAGE=$PREFIX$NAME-$(cat VERSION).zip
+BINARIES="bs1770gain/bs1770gain ffmpeg ffprobe tagit"
+FILES="bs1770gain/ youtube_dl/ VERSION ffmpeg ffprobe tagit handler.py"
 
 # INFO: `youtube-dl` can't find/use non-executable binaries.
 chmod -v +x $BINARIES
@@ -10,7 +11,7 @@ chmod -v +x $BINARIES
 rm -vrf youtube_dl*
 pip3 install --requirement requirements.txt --target .
 
-rm -vf $PACKAGE_PREFIX*.zip
-zip -r $PACKAGE VERSION $BINARIES youtube_dl handler.py
+rm -vf $PREFIX*.zip
+zip -r $PACKAGE $FILES
 
 aws lambda update-function-code --function-name $NAME --zip-file fileb://$PACKAGE
